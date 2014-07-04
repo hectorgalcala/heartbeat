c856adb68947dc8ad25ca4ae6a025b0ea0f1e507
hmgaar <hector@Miguels-MacBook-Air.local>
2014-07-04 13:26:01 -0400

    4th of July changes 8

--- app/models/club.rb
+++ app/models/club.rb
@@ -41,13 +41,13 @@ elsif hb > 40
   return "Chilling"
 elsif hb > 20
   return "Almost empty come with friends!"
-#elsif hb >  0
-  #return "Definitely come with friends or else you are on your own"
-elsif hb == 15
+elsif hb >  10
+  return "Definitely come with friends or else you are on your own"
+elsif hb == 5
   return "TONIGHT IS GONNA BE A GOOD NIGHT"
-elsif hb == 16
+elsif hb == 6
   return "WILD ON"
-elsif hb == 17
+elsif hb == 7
   return "OF THE HOOK"
 else
   "The place is close as of now"
